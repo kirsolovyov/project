992c3a0323351ad185099b7f39e7d5d78956e399
Kirill <example@gmail.com>
2024-07-24 17:36:08 +0300

    solved 1st task

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 "use strict";
 
-console.log(5);
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  private: false,
+};
+
+const lastFilmName = prompt("Один из последних посмотренных фильмов?", ""),
+  howRateHim = +prompt("На сколько цените его?", "");
+
+personalMovieDB.movies[lastFilmName] = howRateHim;
+
+console.log(personalMovieDB);
